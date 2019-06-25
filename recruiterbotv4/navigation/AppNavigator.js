af8354204e1490e5340c6a6a2ce34e815bc0d7bb
joesav1 <joe.m.savidge@gmail.com>
2019-06-25 12:37:14 +0100

    Populated loginpage with neccessary code

--- recruiterbotv4/navigation/AppNavigator.js
+++ recruiterbotv4/navigation/AppNavigator.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 
-import TestComponent from '../components/AppComponents/TestComponent';
+import LoginPage from '../screens/main/loginPage';
 import rHomepage from '../screens/Recruiter/rHomepage';
 
 const Rootstack = createStackNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    TestComponent: { screen: TestComponent},
+    LoginPage: { screen: LoginPage},
     rHomepage: { screen: rHomepage }
   });
 
---
+++ recruiterbotv4/screens/main/loginPage.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+import { Text, View, TextInput, StyleSheet, Button} from 'react-native'
+//import { Container, Item, Form, Input, Button, Label } from "native-base";
+import firebase from 'firebase';
+//import ApiKeys from '../../constants/ApiKeys';
+
+
+import AppNavigator from './../../navigation/AppNavigator';
+
+
+export class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+
+            };
+        }
+    
+        Signup = (email,password) => {
+            try {
+                firebase 
+                    .auth()
+                    .createUserWithEmailAndPassword(email,password)
+                    .then(user => {
+                        console.log("User created");
+                    });
+                //this.props.navigation.navigate('rHomepage')
+                } catch(error) {
+                console.log(error.toString(error))
+    
+            };
+        }
+    
+        Login = (email, password) => {
+            try {
+                firebase
+                    .auth()
+                    .signInWithEmailAndPassword(email, password)
+                    .then(res => {
+                        console.log(res.user.email)
+                    });
+            } catch(error) {
+                console.log("Couldnt login")
+            } 
+        }
+    
+    render() {
+        //console.log("Checking debugger -JS")
+        return (
+            <View>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: 10}}
+                    onChangeText = {email => this.setState({ email }) }
+                />
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: 10}}
+                    onChangeText = {password => this.setState({ password }) }
+                />
+                <Button 
+                    title = "Login"
+                    color = "#841584"
+                    onPress = {() => this.Login(this.state.email, this.state.password)}
+                    style ={{margin: 10}}
+                />
+                <Button 
+                    title = "Sign-up"
+                    color = '#FF5733'
+                    onPress = {() => this.Signup(this.state.email, this.state.password)}
+                    style ={{margin: 10}}
+                />
+                <Button 
+                    title = "test-screens"
+                    color = '#FF5733'
+                    onPress = {() => this.props.navigation.navigate('rHomepage')}
+                    style ={{margin: 10}}
+                />
+
+            </View>
+        )
+    }
+}
+
+export default LoginPage
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      // alignItems: "center",
+      justifyContent: "center"
+    }
+  });
