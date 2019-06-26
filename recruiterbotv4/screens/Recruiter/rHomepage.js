d757f70fba6d9275c519ccc2db0e7d61cbe4c2d3
joesav1 <joe.m.savidge@gmail.com>
2019-06-26 12:16:08 +0100

    checking in homepage that user is still logged in

--- recruiterbotv4/screens/Recruiter/rHomepage.js
+++ recruiterbotv4/screens/Recruiter/rHomepage.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import firebase from 'firebase';
 
 export class rHomepage extends Component {
-    render() {
-        return (
-            <View>
-                <Text> Welcome to the recruiter homepage! </Text>
-            </View>
-        )
+    constructor() {
+        super();
+        this.state = {
+          loading: true,
+          authenticated: false,
+        };
+      }
+    
+      componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+            this.setState({ loading: false, authenticated: true });
+          } else {
+            this.setState({ loading: false, authenticated: false });
+          }
+        });
+      }
+    
+      render() {
+        if (this.state.loading) {
+            console.log("nobody is logged in -js")
+            return null;
+            } // Render loading/splash screen etc
+    
+        if (!this.state.authenticated) {
+          console.log("nobody is logeed in still! -js")
+            return null;
+            }
+        console.log("Checking again to make sure the session is active -js")
+        console.log(this.state.authenticated)
+        return <Text> Yes it worked, welcome to the homepage!</Text>
+      }
     }
-}
-
 export default rHomepage
