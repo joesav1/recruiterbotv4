cb722063439dfcad407dba3483ed41b712bdd85c
joesav1 <joe.m.savidge@gmail.com>
2019-07-29 17:03:33 +0100

    can finally interact with the personality insights cloud function

--- recruiterbotv4/screens/chatbot/chatbotHoldingPage.js
+++ recruiterbotv4/screens/chatbot/chatbotHoldingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Button } from 'react-native'
+import { personalityRequest } from './personality';
 
 export class chatbotHoldingPage extends Component {
     constructor(props) {
@@ -8,6 +9,7 @@ export class chatbotHoldingPage extends Component {
         this.state = {
             finalText1: null,
             messagesMain1: props.navigation.state.params.messagesPass,
+
             
         }
     }
@@ -18,42 +20,56 @@ export class chatbotHoldingPage extends Component {
         var textOnlyMessages = [];
         var finalTextOutput = null;
         for( const member of rawMessages ){
-            console.log("Chekcing i")
-            console.log(member)
-            console.log("end of i check")
-            console.log("checking i.user")
-            console.log(member.user)
-            console.log("Checking i.user._id")
-            console.log(member.user._id)
-            console.log("End of checks")
+            // console.log("Chekcing i")
+            // console.log(member)
+            // console.log("end of i check")
+            // console.log("checking i.user")
+            // console.log(member.user)
+            // console.log("Checking i.user._id")
+            // console.log(member.user._id)
+            // console.log("End of checks")
             if( member.user._id !== "2" ) {
                 filteredMessages.push(member)
             }
         }
-        console.log("checking filteredMessages")
-        console.log(filteredMessages)
-        console.log("end of filteredMessages check")
+        // console.log("checking filteredMessages")
+        // console.log(filteredMessages)
+        // console.log("end of filteredMessages check")
         for( const text of filteredMessages) {
 
             textOnlyMessages.push(text.text)
         }
-        console.log("checking textOnlyMessages")
-        console.log(textOnlyMessages)
-        console.log("end of textOnlyMessages check")
+        // console.log("checking textOnlyMessages")
+        // console.log(textOnlyMessages)
+        // console.log("end of textOnlyMessages check")
+        textOnlyMessages.pop()
         finalTextOutput = textOnlyMessages.join(' ');
-        console.log("checking finalTextOutput")
-        console.log(finalTextOutput)
-        console.log("end of finalTextOutput check")
-        this.setState({finalText1: finalTextOutput})
-        console.log("checking this.state.finalText")
-        console.log(this.state.finalText1)
-        console.log("end of finalText check")
+        // console.log("checking finalTextOutput")
+        // console.log(finalTextOutput)
+        // console.log("end of finalTextOutput check")
+ 
+        this.setState({finalText1: finalTextOutput});
+        // console.log("checking this.state.finalText")
+        // console.log(this.state.finalText1)
+        // console.log("end of finalText check")
 
 
     }
 
     componentDidMount() {
         this.prepareMessages()
+        console.log("checking this.state.finalText in componentdidMount")
+        console.log(this.state.finalText1)
+        console.log("end of finalText check in the componentdidMount")
+        
+    }
+
+    startPersonality = async() => {
+        let response = await personalityRequest(this.state.finalText1)
+        console.log("chatbotholdingpage seeing if response does anything")
+        console.log(response)
+        console.log("end of chatbotholdingpage, startPersonality repsonse check")
+
     }
 
 
@@ -64,10 +80,18 @@ export class chatbotHoldingPage extends Component {
         console.log("checking this.state.finalText in the render")
         console.log(this.state.finalText1)
         console.log("end of finalText check in the render")
+
         return (
             <View>
                 <Text> textInComponent </Text>
+                <Button
+                    title = "Test personality"
+                    color = "#841584"
+                    onPress ={()=>this.startPersonality()}       
+                    style={{margin: 10}}         
+                />
             </View>
+            
         )
     }
 }
--- recruiterbotv4/screens/chatbot/chatbotShell.js
+++ recruiterbotv4/screens/chatbot/chatbotShell.js
@@ -60,7 +60,7 @@ export class chatbotShell extends Component {
         accessibilityLabel='main'
       >
         <CountDown style={{marginTop: 80}}
-            until={30}
+            until={15}
             size = {20}
             timeToShow={['M','S']}
             onFinish={() => {this.endOfTimer()}}
